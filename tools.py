843cbaa9f9d5a10e11c3d401df6ec629d5ee3f49
Bolster2k <paul.delhom0@gmail.com>
2025-03-03 17:24:20 +0100

    adapted inner-iteration to the new outer-iterations

--- tools.py
+++ tools.py
@@ -136,6 +136,48 @@ def detect_peaks(correlation, correlation_bis, threshold=0.8, min_gap=200):
 
     return res, res2
 
+
+def detect_peaks_inner(correlation, threshold=0.9, min_gap=1, keep='max'):
+    """Detects peaks in a correlation result.
+    
+       Consecutive high values are omitted if the gap between the index is
+       smaller than `min_gap`.
+       The `keep` argument can take different values depending on which sample
+       should be kept in case more than a single sample reach the threshold
+       consecutively.
+       
+       Args
+           - correlation (numpy.array): the correlation result.
+       
+       Kwargs
+           - threshold (float): the threshold above which a peak can be detected.
+           - min_gap (int): the minimum distance between two peaks (default to 1).
+           - keep (str): can be one in ('max','first','last') which respectively
+             keeps the sample corresponding to the maximum value of the interval,
+             keeps the first sample from the interval, keeps the last sample from
+             the interval.
+       
+       Returns
+           A list of peak abscissas.
+       """
+    detected_peaks = np.where(correlation>threshold)[0]
+    if len(detected_peaks) == 0:
+        return []
+    else:
+        current_max = 0
+        res = [detected_peaks[0]]
+        for p in detected_peaks[1:]:
+            if (p-res[-1]) > min_gap:
+                res.append(p)
+                current_max = correlation[p]
+            else: # we are in an interval of consecutive samples reaching threshold
+                if keep == 'last':
+                    res[-1] = p
+                elif keep == 'max' and correlation[p] > current_max:
+                    res[-1] = p
+                    current_max = correlation[p]
+    return res
+
 def detect_first_peak(pcc,peaks,wrange=20):
     """Compute the first (undetected) peak when he is smaller than the others.
 
